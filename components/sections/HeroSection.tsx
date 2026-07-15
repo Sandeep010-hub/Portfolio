40d767faf880aac5820caf74458bc195068f84ed
Yaswanth Sandeep Sai Kalagatla <135518346+Sandeep010-hub@users.noreply.github.com>
2026-07-15 15:29:45 +0530

    Resume Updated2

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -211,7 +211,7 @@ const HeroSection = () => {
             </motion.button>
 
             <motion.a
-              href="https://drive.google.com/file/d/16MMExhs3ib2WTPm_BZJI3O_56sLrVc04/view?usp=sharing"
+              href="https://drive.google.com/file/d/1u6LfAMom_iz6ZMz5lIolsgIMTLjTqjQZ/view"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
